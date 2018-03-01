e1469538d904cbc7b1d3f07ef052ce662984e060
AIML <wiliam.chen52@gmail.com>
2018-03-02 00:58:54 +0800

    update blog index page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.filter(publish_time__lte=timezone.now()).order_by('publish_time')
+    return render(request, 'blog/post_list.html',{'posts':posts})
